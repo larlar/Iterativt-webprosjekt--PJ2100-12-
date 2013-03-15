21465625d939086ef47235a03e3ef12774213309
Rolf-Erik Christensen <chrrol12@nith.no>
2013-03-15 11:59:54 +0100

    Changed SQLDetails.java with updated information

--- webtemplate/src/main/java/no/nith/SQLDetails.java
+++ webtemplate/src/main/java/no/nith/SQLDetails.java
@@ -4,8 +4,8 @@ public class SQLDetails {
 	
 	
 	//URL, brukernavn og passord til SQL connect.
-	public static String url = "jdbc:mysql://localhost/daredigital";
-	public static String user = "daredig";
-	public static String password = "D4repass";
+	public static String url = "jdbc:mysql://mysql.nith.no/larlar12";
+	public static String user = "larlar12";
+	public static String password = "larlar12";
 
 }
