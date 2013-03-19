0ee351f7b6e75561782cf6ad06f2b5771b9f270d
Jon Jahren <jon@ask>
2013-03-19 15:56:41 +0100

    Renamed two files to prepare them for queries

---
+++ darematchmaking.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Dare Matchmaking</title>
+	<link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+<!-- Content wrapper -->
+    <div id="wrapper">
+<!-- Banner Top -->
+	<header id="top">
+		<div><img src="img/header_nordic1.jpg" alt="banner top"/></div>
+	</header>
+
+<!-- navi menue top -->
+	<div class="navi-top cf">
+    <ul>
+        <li class="navi-object Home"><a href="index.html">Home</a></li>
+        <li class="navi-object About"><a href="about.html">About</a></li>
+        <li class="navi-object Apply"><a href="apply.html">Apply</a></li>
+		<li class="navi-object Dare Members"><a href="daremembers.html">Dare Members</a></li>
+		<li class="navi-object Dare Matchmaking"><a href="darematchmaking.html">Dare Matchmaking</a></li>
+		<li class="navi-object Contact"><a href="contact.html">Contact</a></li>
+        <li class="search"><form >
+            <input type="text" />
+                <button class="submit">Go</button>
+        </form></li>
+    </ul>
+</div>
+	
+	<div class="content cf">
+		<div class="primary-content">
+        <section id="welcomeHead" class="article">
+			<h1>Dare Teams</h1>
+			
+		
+		</div><!-- primairy content end-->
+	
+	      <div class="sidebar">
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fdarenordic&amp;width=320&amp;height=558&amp;show_faces=true&amp;colorscheme=light&amp;stream=true&amp;border_color&amp;header=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:320px; height:558px;" allowTransparency="true"></iframe>				<div class ="nith_add">
+					<div class ="nith_add">
+					<img src="img/nithbutton.jpg" alt="NITH add">
+				</div>
+				<div class="dareinternational_add">
+					<img src="img/dareint.jpg" alt="Dare international add">
+				</div>
+				</div>
+			</div><!-- sidebar end -->	
+	</div><!-- content end -->
+	</div><!-- wrapper end -->
+	
+	<!-- footer wraper -->
+	<div class="footer">
+		<center>All images, videos, content Copyright © 2013 Dare to be Digital Limited except where noted. All Rights Reserved. <a href="mailto:enquiries@daretobedigital.com">enquiries@daretobedigital.com</a> <br />
+		Website Design by MTC Media | Dare 2011 illustrations by Sooper Double D</center>
+	</div><!-- footer end -->
+	
+</body>
+
+</html>
