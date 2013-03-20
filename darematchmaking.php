cf09416013c62c29b9ddfc90135689b6bc6f8c81
Lars Larsen Skjveland <larlar12@nith.no>
2013-03-20 16:59:00 +0100

    rydding i members og matchmaking kode

--- darematchmaking.php
+++ darematchmaking.php
@@ -5,72 +5,60 @@
     <title>Dare Matchmaking</title>
 	<link rel="stylesheet" href="css/main.css" />
 </head>
-
 <body>
-<!-- Content wrapper -->
-    <div id="wrapper">
-<!-- Banner Top -->
-	<header id="top">
-		<div><img src="img/header_nordic1.jpg" alt="banner top"/></div>
-	</header>
+	<!-- Content wrapper -->
+	<div id="wrapper">
+		<!-- Banner Top -->
+		<header id="top">
+			<div><img src="img/header_nordic1.jpg" alt="banner top"/></div>
+		</header>
 
-<!-- navi menue top -->
-	<div class="navi-top cf">
-	<?php
-		include ('PHP/navigation/navi-bar.php');
-	?>
-</div>
-	
-	<div class="content cf">
-		<div class="primary-content">
-        <section id="welcomeHead" class="article">
-			<h1>Dare Teams</h1>
-			<form action="darematchmaking.php" method="get">			
-
-			<select id="roles" name="roles" action="darematchmaking.php">
-			<option value="empty"></option>
-<?php
-	//Makes a dropdown list based on roles in the MySQL database
-	include ('PHP/Dropdown/roles.php');
-?>
-
-</select>
-<select id="country" name="country">
-<option>All</option>
-<?php
-	//Makes a dropdown list based on countires in the MySQL database
-	include ('PHP/Dropdown/countries.php');
-?>
-			</select>
-
-<input type="submit">
-</form>
-<table class="searchoutput">
-	<tr>
-		<th>Team</th>
-		<th>Name</th>
-		<th>Role</th>
-	</tr>	
-<?php 
-	//Prints the results from the matchmaking query based on the values in the dropdown lists
-	include ('PHP/search/matchmaking.php');
-	
-	while($row = mysqli_fetch_array($results)) {
-		echo "<tr><td>" . $row['Team_Name'] . "</td><td>" . $row['Name'] . "</td><td>" . $row['Role'] . "</td></tr>"; 
-	}
-?>			
-	</table>	
-		</div><!-- primairy content end-->
-	
-	</div><!-- content end -->
+		<!-- navi menue top -->
+		<div class="navi-top cf">
+			<?php include ('PHP/navigation/navi-bar.php'); ?>
+		</div>
+		<div class="content cf">
+			<div class="primary-content">
+				<h1>Dare Teams</h1>
+				<form action="darematchmaking.php" method="get">			
+					<select id="roles" name="roles" action="darematchmaking.php">
+						<option value="empty"></option>
+						<?php
+							//Makes a dropdown list based on roles in the MySQL database
+							include ('PHP/Dropdown/roles.php');
+						?>
+					</select>
+					<select id="country" name="country">
+						<option>All</option>
+						<?php
+							//Makes a dropdown list based on countires in the MySQL database
+							include ('PHP/Dropdown/countries.php');
+						?>
+					</select>
+					<input type="submit">
+				</form>
+				<table class="searchoutput">
+					<tr>
+						<th>Team</th>
+						<th>Name</th>
+						<th>Role</th>
+					</tr>	
+					<?php 
+						//Prints the results from the matchmaking query based on the values in the dropdown lists
+						include ('PHP/search/matchmaking.php');
+						
+						while($row = mysqli_fetch_array($results)) {
+							echo "<tr><td>" . $row['Team_Name'] . "</td><td>" . $row['Name'] . "</td><td>" . $row['Role'] . "</td></tr>"; 
+						}
+					?>			
+				</table>	
+			</div><!-- primairy content end-->
+		</div><!-- content end -->
 	</div><!-- wrapper end -->
-	
 	<!-- footer wraper -->
 	<div class="footer">
 		<center>All images, videos, content Copyright © 2013 Dare to be Digital Limited except where noted. All Rights Reserved. <a href="mailto:enquiries@daretobedigital.com">enquiries@daretobedigital.com</a> <br />
 		Website Design by MTC Media | Dare 2011 illustrations by Sooper Double D</center>
-	</div><!-- footer end -->
-	
+	</div><!-- footer end -->	
 </body>
-
 </html>
--- daremembers.php
+++ daremembers.php
@@ -5,84 +5,65 @@
     <title>Dare Members</title>
 	<link rel="stylesheet" href="css/main.css" />
 </head>
-
 <body>
-<!-- Content wrapper -->
+	<!-- Content wrapper -->
     <div id="wrapper">
-<!-- Banner Top -->
-	<header id="top">
-		<div><img src="img/header_nordic1.jpg" alt="banner top"/></div>
-	</header>
-
-<!-- navi menue top -->
-	<div class="navi-top cf">
-	<?php
-		include ('PHP/navigation/navi-bar.php');
-	?>
-</div>
-	
-	<div class="content cf">
-		<div class="primary-content">
-		<h1>Dare Members</h1>
-		
-
-		<form action="daremembers.php" method="get">
-		Firstname: <input type="text" name="textValue" Placeholder="Search">
-		<input type="submit">
-
-
-			<select id="country" name="country">
-			<option>All</option>
-<?php
-	include ('PHP/Dropdown/countries.php');
-?>
-</select>
-
-
-<p2>Roles</p2>
-<select id="roles" name="roles" action="daremembers.php">
-<option>All</option>
-
-<?php
-	include ('PHP/Dropdown/roles.php');
-?>
-
-</select>
-
-<select id="hasteam" name="hasteam" action="daremembers.php">
-Har du lag?
-<option value="empty"></option>
-<option value="yes">Yes</option>
-<option value="no">No</option>
-</select>
-
-</form>
-<table class="searchoutput">
-	<tr>
-		<th>Lastname</th>
-		<th>Firstname</th>
-		<th>Role</th>
-		<th>Team</th>
-	</tr>
-	<?php 
-		//Prints the results from the searchfunction query, based on values from the search boxes
-		include ('PHP/search/searchfunction.php');
-		
-		while($row = mysqli_fetch_array($results)) {
-			echo "<tr><td>" . $row['Lastname'] . "</td><td>" . " " . $row['Firstname'] . "</td><td>" . $row['Role'] . "</td><td>" . $row['Team'] . "</td></tr>"; 
-		}
-	?>
-	</table>	
-		</div><!-- primary content end-->	
-	</div><!-- content end -->
-	</div><!-- wrapper end -->
-	
+		<!-- Banner Top -->
+		<header id="top">
+			<div><img src="img/header_nordic1.jpg" alt="banner top"/></div>
+		</header>
+		<!-- navi menue top -->
+		<div class="navi-top cf">
+			<?php include ('PHP/navigation/navi-bar.php');?>
+		</div>
+		<!-- content holder -->
+		<div class="content cf">
+			<!-- primary content -->
+			<div class="primary-content">
+				<h1>Dare Members</h1>
+					<form action="daremembers.php" method="get">
+						Firstname: <input type="text" name="textValue" Placeholder="Search">
+						<input type="submit">
+						<select id="country" name="country">
+							<option>All</option>
+								<?php include ('PHP/Dropdown/countries.php');?>
+						</select>
+								
+						<p2>Roles</p2>
+						<select id="roles" name="roles" action="daremembers.php">
+							<option>All</option>
+								<?php include ('PHP/Dropdown/roles.php');?>
+						</select>
+						<select id="hasteam" name="hasteam" action="daremembers.php">
+							Har du lag?
+							<option value="empty"></option>
+							<option value="yes">Yes</option>
+							<option value="no">No</option>
+						</select>
+					</form>
+					<table class="searchoutput">
+						<tr>
+							<th>Lastname</th>
+							<th>Firstname</th>
+							<th>Role</th>
+							<th>Team</th>
+						</tr>
+						<?php 
+							//Prints the results from the searchfunction query, based on values from the search boxes
+							include ('PHP/search/searchfunction.php');
+							
+							while($row = mysqli_fetch_array($results)) {
+								echo "<tr><td>" . $row['Lastname'] . "</td><td>" . " " . $row['Firstname'] . "</td><td>" . $row['Role'] . "</td><td>" . $row['Team'] . "</td></tr>"; 
+							}
+						?>
+					</table>	
+			</div><!-- primary content end-->	
+		</div><!-- content end -->
+	</div><!-- wrapper end -->	
 	<!-- footer wraper -->
 	<div class="footer">
 		<center>All images, videos, content Copyright © 2013 Dare to be Digital Limited except where noted. All Rights Reserved. <a href="mailto:enquiries@daretobedigital.com">enquiries@daretobedigital.com</a> <br />
 		Website Design by MTC Media | Dare 2011 illustrations by Sooper Double D</center>
 	</div><!-- footer end -->
-	
 </body>
-
 </html>
