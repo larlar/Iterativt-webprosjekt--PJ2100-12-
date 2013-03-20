1a5920b66b168f1eead1501a96595cac9deeccb4
Rolf-Erik Christensen <chrrol12@nith.no>
2013-03-20 16:17:34 +0100

    Changed connection info in DB

--- PHP/Database/DBconnection.php
+++ PHP/Database/DBconnection.php
@@ -1,3 +1,9 @@
 <?php
+<<<<<<< HEAD
 	$connection = mysqli_connect("localhost", "daredig", "D4repass", "daredigital");
+=======
+	//URL, username, password, dbname
+	$connection = mysqli_connect("mysql.nith.no", "chrrol12", "chrrol12", "chrrol12");
+
+>>>>>>> Filer endret til php
 ?>
