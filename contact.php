ab1a87f03a5cb82e1355d22f68912906aee86fc6
Rolf-Erik Christensen <chrrol12@nith.no>
2013-03-20 16:16:52 +0100

    Renamed html files to PHP and added some code

--- contact.php
+++ contact.php
@@ -14,17 +14,9 @@
 		</header>
 <!-- navi menue top -->
 		<div class="navi-top cf">
-			<ul>
-				<li class="navi-object Home"><a href="index.html">Home</a></li>
-				<li class="navi-object About"><a href="about.html">About</a></li>
-				<li class="navi-object Apply"><a href="apply.html">Apply</a></li>
-				<li class="navi-object Dare Members"><a href="daremembers.php">Dare Members</a></li>
-				<li class="navi-object Dare Matchmaking"><a href="darematchmaking.php">Dare Matchmaking</a></li>
-				<li class="navi-object Contact"><a href="contact.html">Contact</a></li>
-				<li class="search"><form action="daremembers.php" method="get">
-					<input type="text" name="textValue" Placeholder="Search for members"/>
-					<button class="submit" type="submit">Search</button>
-			</ul>
+			<?php
+				include ('PHP/navigation/navi-bar.php');
+			?>
 		</div>
 		<!-- content holder -->	
 		<div class="content cf">
