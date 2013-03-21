e5bc30e173b7b03c8dd2f9a597b49ffcf6fb4041
Lars Larsen Skjveland <larlar12@nith.no>
2013-03-21 09:58:19 +0100

    la til norsk i navigasjonsmeny på norsk

--- PHP/navigation/navi-bar-nor.php
+++ PHP/navigation/navi-bar-nor.php
@@ -1,12 +1,12 @@
 <ul> 
-	<li class="navi-object Home"><a href="index-nor.php">Home</a></li> 
-	<li class="navi-object About"><a href="about-nor.php">About</a></li> 
+	<li class="navi-object Home"><a href="index-nor.php">Hjem</a></li> 
+	<li class="navi-object About"><a href="about-nor.php">Om</a></li> 
 	<li class="navi-object Apply"><a href="apply-nor.php">Apply</a></li> 
-	<li class="navi-object Dare Members"><a href="daremembers.php">Dare Members</a></li> 
+	<li class="navi-object Dare Members"><a href="daremembers.php">Dare Medlemmer</a></li> 
 	<li class="navi-object Dare Matchmaking"><a href="darematchmaking.php">Dare Matchmaking</a></li> 
-	<li class="navi-object Contact"><a href="contact-nor.php">Contact</a></li> 
+	<li class="navi-object Contact"><a href="contact-nor.php">Kontakt</a></li> 
 	<li class="search"><form action="daremembers-nor.php" method="get"> 
 		<input type="text" name="textValue" Placeholder="Søk etter medlemmer"/> 
-			<button class="submit" type="submit">Search</button> 
+			<button class="submit" type="submit">Søk</button> 
 	</form></li> 
 </ul>
