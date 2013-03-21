1af9b5f0ea66fb059eeb6c1b33ea53f0d137baae
Lars Larsen Skjveland <larlar12@nith.no>
2013-03-21 16:17:49 +0100

    opprettet norske søkesider og linket navi til nor

---
+++ darematchmaking-nor.php
@@ -0,0 +1,47 @@
+﻿<?php include("header-nor.php") ?>
+		<div class="content cf">
+			<div class="primary-content">
+				<h1>Dare Matchmaking</h1>
+				<form action="darematchmaking.php" method="get">			
+					<select id="roles" name="roles" action="darematchmaking.php">
+						<option value="empty">Rolle</option>
+						<?php
+							//Makes a dropdown list based on roles in the MySQL database
+							include ('PHP/Dropdown/roles.php');
+						?>
+					</select>
+					<select id="country" name="country">
+						<option value="All">Alle land</option>
+						<?php
+							//Makes a dropdown list based on countires in the MySQL database
+							include ('PHP/Dropdown/countries.php');
+						?>
+					</select>
+					<button type="submit" class="submit">Søk</button>
+				</form>
+				<table class="searchoutput">
+					<tr>
+						<th>Lag</th>
+						<th>Navn</th>
+						<th>Rolle</th>
+						<th>Email</th>
+					</tr>	
+					<?php 
+						//Prints the results from the matchmaking query based on the values in the dropdown lists
+						include ('PHP/search/matchmaking.php');
+						
+						while($row = mysqli_fetch_array($results)) {
+							echo "<tr><td>" . $row['Team_Name'] . "</td><td>" . $row['Name'] . "</td><td>" . $row['Role'] . "</td><td>" . $row['Email'] . "</td></tr>"; 
+						}
+					?>			
+				</table>	
+			</div><!-- primairy content end-->
+		</div><!-- content end -->
+	</div><!-- wrapper end -->
+	<!-- footer wraper -->
+	<div class="footer">
+		<center>All images, videos, content Copyright © 2013 Dare to be Digital Limited except where noted. All Rights Reserved. <a href="mailto:enquiries@daretobedigital.com">enquiries@daretobedigital.com</a> <br />
+		Website Design by MTC Media | Dare 2011 illustrations by Sooper Double D</center>
+	</div><!-- footer end -->	
+</body>
+</html>
---
+++ daremembers-nor.php
@@ -0,0 +1,53 @@
+﻿<?php include("header-nor.php") ?>
+		<!-- content holder -->
+		<div class="content cf">
+		<!-- primary content -->
+			<h1>Dare Medlemmer</h1>
+				<form action="daremembers.php" method="get">
+					<p></p><input type="text" name="textValue" Placeholder="Search">
+					<select id="country" name="country">
+						<option value="All">Alle land</option>
+							<?php include ('PHP/Dropdown/countries.php');?>
+					</select>
+							
+					
+					<select id="roles" name="roles" action="daremembers.php">
+						<option value="All">Alle Roller</option>
+							<?php include ('PHP/Dropdown/roles.php');?>
+					</select>
+					<select id="hasteam" name="hasteam" action="daremembers.php">
+						<option value="empty">Har Lag</option>
+						<option value="yes">Ja</option>
+						<option value="no">Nei</option>
+					</select>
+					<button type="submit" class="submit">Søk</button>
+				</form>
+				<table class="searchoutput">
+					<tr>
+						<th>Etternavn</th>
+						<th>Fornavn</th>
+						<th>Email</th>
+						<th>Rolle</th>
+						<th>Lag</th>
+						<th>Land</th>
+					</tr>
+					<?php 
+						//Prints the results from the searchfunction query, based on values from the search boxes
+						include ('PHP/search/searchfunction.php');
+						
+						while($row = mysqli_fetch_array($results)) {
+							echo "<tr><td>" . $row['Lastname'] . "</td><td>" . $row['Firstname'] . "</td><td>" 
+							. $row['Email'] . "</td><td>" . $row['Role'] . "</td><td>" . $row['Team'] . "</td><td>" . $row['Country']
+							. "</td></tr>"; 
+						}
+					?>
+				</table>	
+		</div><!-- content end -->
+	</div><!-- wrapper end -->	
+	<!-- footer wraper -->
+	<div class="footer">
+		<center>All images, videos, content Copyright © 2013 Dare to be Digital Limited except where noted. All Rights Reserved. <a href="mailto:enquiries@daretobedigital.com">enquiries@daretobedigital.com</a> <br />
+		Website Design by MTC Media | Dare 2011 illustrations by Sooper Double D</center>
+	</div><!-- footer end -->
+</body>
+</html>
